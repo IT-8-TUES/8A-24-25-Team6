75c513b5d2032047593f4f485b1438d21257590b
NotaNumber369 <nanofficialgames@gmail.com>
2025-05-22 06:49:52 +0300

    start of details html?

--- code/cards.js
+++ code/cards.js
@@ -1,27 +1,26 @@
-const cardsData = [
-            {},
-            {
-                title: "BQLA ROZA",
-                artist: "Bulgarian song",
-                genre: "folk",
-                desc: ".",
-                img_title: "bqla roza",
-                img_src: "images/img/BQLA_ROZA.png"
-            },
-            {
-                title: "MQTALO LENCHE QBULKA",
-                artist: "Bulgarian song",
-                genre: "folk",
-                desc: ".",
-                img_title: "dunavsko horo",
-                img_src: "images/img/QBULKA.png"
-            },
-            {
-                title: "DUNAVSKO HORO",
-                artist: "Bulgarian song",
-                genre: "folk",
-                desc: ".",
-                img_title: "dunavsko horo",
-                img_src: "images/img/DUNAVKO_HORO.png"
-            }
-        ];
+const cardsData = {
+    "card-bqla_roza":{
+        title: "BQLA ROZA",
+        artist: "Bulgarian song",
+        genre: "folk",
+        desc: ".",
+        img_title: "bqla roza",
+        img_src: "images/img/BQLA_ROZA.png"
+    },
+    "card-qbulka":{
+        title: "MQTALO LENCHE QBULKA",
+        artist: "Bulgarian song",
+        genre: "folk",
+        desc: ".",
+        img_title: "dunavsko horo",
+        img_src: "images/img/QBULKA.png"
+    },
+    "card-dunavsko":{
+        title: "DUNAVSKO HORO",
+        artist: "Bulgarian song",
+        genre: "folk",
+        desc: ".",
+        img_title: "dunavsko horo",
+        img_src: "images/img/DUNAVKO_HORO.png"
+    }
+};
