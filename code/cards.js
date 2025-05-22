6eeaaa3f7c349d013d5a35b246f2e76a461ed5e1
NotaNumber369 <nanofficialgames@gmail.com>
2025-05-22 06:42:05 +0300

    home page (cards with songs)

---
+++ code/cards.js
@@ -0,0 +1,27 @@
+const cardsData = [
+            {},
+            {
+                title: "BQLA ROZA",
+                artist: "Bulgarian song",
+                genre: "folk",
+                desc: ".",
+                img_title: "bqla roza",
+                img_src: "images/img/BQLA_ROZA.png"
+            },
+            {
+                title: "MQTALO LENCHE QBULKA",
+                artist: "Bulgarian song",
+                genre: "folk",
+                desc: ".",
+                img_title: "dunavsko horo",
+                img_src: "images/img/QBULKA.png"
+            },
+            {
+                title: "DUNAVSKO HORO",
+                artist: "Bulgarian song",
+                genre: "folk",
+                desc: ".",
+                img_title: "dunavsko horo",
+                img_src: "images/img/DUNAVKO_HORO.png"
+            }
+        ];
