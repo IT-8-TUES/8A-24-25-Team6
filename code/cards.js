5cf04ba4504a1241965050d6be80db6d4d5486a1
NotaNumber369 <nanofficialgames@gmail.com>
2025-05-24 02:59:26 +0300

    new songs

--- code/cards.js
+++ code/cards.js
@@ -25,5 +25,41 @@ const cardsData = {
         img_title: "dunavsko horo",
         img_src: "images/img/DUNAVKO_HORO.png",
         audio_src: "audio/DUNAVSKO_HORO.mp3"
+    },
+    "card-esspresso": {
+        title: "ESPRESSO",
+        artist: "Sabrina Carpenter",
+        genre: "pop",
+        desc: "A punchy and addictive summer anthem about confidence and charm",
+        img_title: "espresso",
+        img_src: "images/img/ESPRESSO.png",
+        audio_src: "audio/ESPRESSO.mp3"
+    },
+        "card-die": {
+        title: "DIE WITH A SMILE",
+        artist: "Bruno Mars, Lady Gaga",
+        genre: "pop",
+        desc: "A fantasy collab that oozes retro glam and bittersweet joy",
+        img_title: "die with a smile",
+        img_src: "images/img/DIE_WITH_A_SMILE.png",
+        audio_src: "audio/DIE_WITH_A_SMILE.mp3"
+    },
+    "card-not_like_us": {
+        title: "NOT LIKE US",
+        artist: "Kendrick Lamar",
+        genre: "hip-hop",
+        desc: "A powerful and assertive rap track rooted in West Coast hip-hop tradition",
+        img_title: "not like us",
+        img_src: "images/img/NOT_LIKE_US.png",
+        audio_src: "audio/NOT_LIKE_US.mp3"
+    },
+    "card-magnetic": {
+        title: "MAGNETIC",
+        artist: "Illit",
+        genre: "K-pop",
+        desc: "Bright, bold, and catchy debut song that explores themes of attraction and emotional pull",
+        img_title: "magnetic",
+        img_src: "images/img/MAGNETIC.png",
+        audio_src: "audio/MAGNETIC.mp3"
     }
 };
